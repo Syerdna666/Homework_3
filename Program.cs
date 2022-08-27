b4833a9b04e77d51d35c44e53a89503def236e01
Graceless <syerdnaswag@gmail.com>
2022-08-27 19:37:19 +0300

    Added first task

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// Task_1
+// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+// 14212 -> нет
+
+// 12821 -> да
+
+// 23432 -> да
+
+
+bool IsPalindrome(int number)
+{
+    int reversedNumber = 0;
+    int j = Math.Abs(number);
+    while(j > 0) {
+        reversedNumber = reversedNumber * 10 + j % 10;
+        j = j / 10;
+    }
+    if (Math.Abs(number) == reversedNumber){
+        return true;
+    }
+    else return false;
+}
+
+System.Console.WriteLine(IsPalindrome(12821));
+    
+
+
+
