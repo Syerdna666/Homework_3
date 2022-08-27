4e7b9a39a27e6c3259ce341ed87e0af7d180728a
Graceless <syerdnaswag@gmail.com>
2022-08-27 20:24:17 +0300

    Added task 3

--- Program.cs
+++ Program.cs
@@ -37,3 +37,32 @@ return Math.Round(result, 2);
 }
 
 System.Console.WriteLine(Get3DRange(7, -5 , 0, 1, -1, 9));
+
+
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+void GetCubeTable(int number){
+    for (int i = 1; i <= number; i++){
+        double result = Math.Pow(i, 3);
+        System.Console.Write(result + " ");
+    }
+}
+GetCubeTable(5);
+
+
+
+int[] GetCubes (int n){
+    int[] arr = new int[n];
+
+    for (int i = 1; i <= n; i++){
+        arr[i - 1] = i* i* i;
+    }
+
+    return arr;
+}
+
+foreach (int num in GetCubes(5)){
+    System.Console.Write(num + " ");
+}
